d238b9fc92ecbd5b91c67dbc4c61305310462e52
morris <dltkdah777@gmail.com>
2024-10-07 16:35:08 +0900

    init : start test project

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 # This is a sample Python script.
-import hashlib
-import hmac
 import os
 import time
 from contextlib import asynccontextmanager
-from http.client import HTTPException
-
-import httpx
+from binance.client import Client
 from fastapi import FastAPI
 from dotenv import load_dotenv
-import ccxt
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # response_get_websocket()
-    # key = load_websocket_file()
     print(f'start server')
     yield
     print(f'stop server')
@@ -29,13 +22,6 @@ API_SECRET = os.getenv("BINANCE_API_SECRET")
 
 app = FastAPI(title="block_chain_test", version="0.0.1", lifespan=lifespan, debug=True)
 
-binance = ccxt.binance(config={
-    'apiKey': API_KEY,
-    'secret': API_SECRET
-})
-
-BASE_URL = "https://api.binance.com/api/v3"
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
@@ -44,14 +30,26 @@ def print_hi(name):
 async def read_root():
     return {"message": "Hello World"}
 
-@app.get("/api-check")
+@app.get("/api/key/check")
 async def read_root():
     return {"API Key": API_KEY, "API Secret": API_SECRET}
 
-@app.get("/api/check/account")
-async def check_account():
-    balance = binance.fetch_balance()
-    return balance
+@app.get("/api/account")
+async def get_account_info():
+    try:
+        client = Client(API_KEY, API_SECRET)
+        server_time = client.get_server_time()
+
+        server_timestamp = server_time['serverTime']
+        current_time = int(time.time() * 1000)
+
+        print('Server timestamp:', server_timestamp)
+        print('Current timestamp:', current_time)
+        # account_info = await client.futures_account()
+        # print(account_info)
+        return client.get_account(timestamp=current_time)
+    except Exception as e:
+        print(e)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
